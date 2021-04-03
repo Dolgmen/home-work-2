f73243b5b98cadbe90fb612a1e2a094ef55985c3
Dolgmen <slotsy046@gmail.com>
2021-04-03 20:28:53 +0300

    car 2  ver2

--- proj1_2.rb
+++ proj1_2.rb
@@ -1,9 +1,18 @@
-def mileage (miles_driven,gas_used)
-  return miles_driven / gas_used
+def mileage (miles_driven,gas_used) #,blue_used )
+   miles_driven / gas_used
+   #  miles_driven / blue_used
 end
 
-trip_mileage = mileage (400 , 200)
-puts " ти маєш #{trip_mileage} літрів у цій поїздці"
+#attr_reader :miles
 
-lifetime_milage = mileage (11432 , 366)
-puts "середній розхід #{lifetime_milage} літрів"
+trip_mileage = mileage(400,12)
+puts " ти маєш #{trip_mileage} , літрb у цій поїздці"
+
+lifetime_milage = mileage(11432,366)
+puts "середній розхід #{lifetime_milage} літр"
+
+#rip_mileage2 = mileage2(200,6)
+#puts " ти маєш ,#{rip_mileage2} літрb у цій поїздці"
+
+#ifetime55 = mileage2(11432,115)
+#puts "середній розхід #{ifetime55 } літр"
