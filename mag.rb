1340d020d59a180f41fbe57887095e96f5ee1b2b
Dolgmen <slotsy046@gmail.com>
2021-04-07 22:39:20 +0300

    r5 ver 1

---
+++ mag.rb
@@ -0,0 +1,32 @@
+prices =[2.99, 25.00, 9.99]
+index =0
+while index < prices.length
+  puts prices [index]
+  index += 1
+end
+index = 0
+while index <= prices.length
+  puts prices [index]
+end
+# prices[0]
+# prices[1]
+# prices[2]
+# prices[3] = 153
+# prices[6] = 153
+# puts prices.first
+# puts prices.class
+# puts prices.length
+# puts prices.include? (153)
+# puts prices.find_index(153)
+# p prices
+# # prices.push (0.99)
+# prices.pop
+# prices.shift
+# prices << 5.99
+# p prices
+# puts ["d", "o", "g"].join
+# puts ["d", "o", "g"].join("#")
+# p"d-o-g".chars
+# p "D-o-g".split ("-")
+p prices[prices.length]
+p prices[prices.length - 1]
