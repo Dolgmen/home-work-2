1bfb1eea73c299f0e94b626d26ca164e62dfa6c7
Dolgmen <slotsy046@gmail.com>
2021-04-08 17:29:43 +0300

    block 1 ver 4

--- block.rb
+++ block.rb
@@ -1,20 +1,34 @@
-def my_method(&my_block)
-  puts "were in the metod about to invoke your block!"
-  my_block.call
-  puts "were back in the metod"
-end
-my_method do
-  puts "press \"f\" To pay respect A;exandr!"
-end
+# def my_method(&my_block)
+#   puts "were in the metod about to invoke your block!"
+#   my_block.call
+#   puts "were back in the metod"
+# end
+# my_method do
+#   puts "press \"f\" To pay respect A;exandr!"
+# end
+#
+# def twice (&my_block)
+#   puts " in the medot..."
+#   my_block.call
+#   puts "AC/DC"
+#   my_block.call
+#   puts "aminenm"
+# end
+#
+# twice do
+#   puts "press \"F\" to pay respect Roman "
+# end
 
-def twice (&my_block)
-  puts " in the medot..."
-  my_block.call
-  puts "AC/DC"
-  my_block.call
-  puts "aminenm"
+def give(&my_block)
+  my_block.call("2 turtle doves", "1 partridge")
 end
-
-twice do
-  puts "press \"F\" to pay respect Roman "
+def give
+  yield  "2 turtle doves", "1 partridge"
 end
+def take_this
+  yield "present"
+end
+take_this do |thing|
+  puts "do/end block got #{thing}"
+end
+take_this { |thing| puts  "braces block got #{thing}"}
