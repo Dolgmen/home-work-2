5193263ffac160a03d1b5d3f2a7a4dd329bc9c01
Dolgmen <slotsy046@gmail.com>
2021-04-14 00:31:45 +0300

    april 13 2

--- gufy.rb
+++ gufy.rb
@@ -1,7 +1,16 @@
 # review_file = File.open("gufy.txt")
-# lines = review_file.readlines
-# review_file.close
-# puts "Line 4: #{lines[3]}"
+# string = reviews.first
+# words = string.split(" ")
+# p words
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
+ # reviews = relevant_lines.reject {|line| line.include ? ("--")}
+
+# # puts "Line 4: #{lines[3]}"
 # puts "Line 1: #{lines[0]}"
 # review_file.close
 
@@ -67,3 +76,39 @@ make_casserole do
   "rice, broccoli, and chicken"
 end
 
+p [1,2,3,4,5,6,884,883].find_all {|number| number.even?}
+p [1,2,3,4,5,6,884,883].find_all {|number| number.odd?}
+
+p ['a', 'b', 'c'].find_all { |item|false }
+
+def find_all
+matching_items = []
+self.each do |item|
+  if yield(item)
+    matching_items << item
+  end
+end
+end
+
+p "1-800-555-0199".split("-")
+p "his/her".split("/")
+p "apple, avocado, anvil".split(", ")
+p "alhad-bar , .com/OW ".split(",")
+p "alhad-bar , .com/OW ".split("-")
+p "alhad-bar , .com/OW ".split("")
+p "alhad-bar , .com/OW ".split(" ")
+# string = review_file.first
+# words = string.split(" ")
+# p words
+p ["1", "800", "555", "0199"].find_index("800")
+p ["his", "her"].find_index("his")
+p ["apple", "avocado", "anvil"].find_index(" ")
+
+squares = [10,20,45,65].map { |number| number ** 2 }
+cubes = [2,3,4].map { |number|number ** 3 }
+area_codes = ['8-800-5-5-5-5-3-8','25-8800-645-26548-55' ].map do |phone|
+  phone.split("-")[0...2]
+end
+p squares, cubes, area_codes
+adjectives = reviews.map { |review| find_adjective(review) }
+# p adjectives
